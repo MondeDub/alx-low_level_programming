2bd45c7debe1b84e8831b746e076519b8e2ab798
MondeDub <Mondedubazane5@gmail.com>
2023-02-23 01:13:22 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_numbers -Function that prints the numbers from 0123456789\n
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x= '0'; x <= '10'; x++)
+}
+_putchar(c);
+}
+_putchar('\n');
+}
