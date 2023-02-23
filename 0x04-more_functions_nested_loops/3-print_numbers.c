f2c6174e12bfb8b5e2ce775e95d1fdefce325353
MondeDub <Mondedubazane5@gmail.com>
2023-02-23 08:39:31 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - Function that prints the numbers from 0-9.
+ * print_numbers -prints the numbers from 0-9.
  */
 
 void print_numbers(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+for (n = 0; n <= 9; n++)
 	{
-	_putchar((n % 10), +'0');
+_putchar((n % 10),  +0);
 	}
-	_putchar('\n')i;
+_putchar('\n')i;
 }
