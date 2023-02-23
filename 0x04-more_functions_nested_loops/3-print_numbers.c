de657a2035b6353422a19de1e39ec590b25f2dca
MondeDub <Mondedubazane5@gmail.com>
2023-02-23 04:12:20 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _print_numbers -Function that prints the numbers from 0123456789\n
- *
- * Return: void
+ * _print_numbers -Function that prints the numbers from 0-9.
  */
 
 void print_numbers(void)
 {
 	int x;
 
-	for (x= '0'; x <= '10'; x++)
-}
-_putchar(c);
-}
-_putchar('\n');
+	for (x = '0'; x <= '9'; x++)
+	{
+	_putchar((x % 10); +'0')
+	}
+	_putchar('\n');
 }
