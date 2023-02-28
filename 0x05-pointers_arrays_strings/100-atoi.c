82d113c57e0f3abe9d3045ea313b6bb9abe07429
MondeDub <Mondedubazane5@gmail.com>
2023-02-28 01:40:38 -0800

    random valid passwords

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _atoi- converts a string to an integer
+ * atoi- converts a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
  */
-int _atoi(char *s)
+int atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
@@ -33,7 +33,7 @@ int _atoi(char *s)
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			f = 0
+			f = 0;
 		}
 		i++;
 	}
