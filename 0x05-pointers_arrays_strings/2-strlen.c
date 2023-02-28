e6e74acb5182b494c6f5882af7c0dee477fc599b
MondeDub <Mondedubazane5@gmail.com>
2023-02-27 16:48:59 -0800

    returns length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 /**
  * _strlen - returns the length of a string
  * @s: string
- * Return: Length
+ * Return: length
  */
 int _strlen(char *s)
 {
 	int longi = 0;
 
-	while (*!= '\0')
+	while (*s != '\0')
 	{
 		longi++;
 		s++;
