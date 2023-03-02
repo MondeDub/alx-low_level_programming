75e081d3756b3641fcd81a3d766cff7066feb9f4
MondeDub <Mondedubazane5@gmail.com>
2023-03-02 09:58:46 -0800

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reverse array - reverse array of integers
+ * reverse_array - reverse array of integers
  * @a: array
  * @n: number of elements of array
  * Return: void
  */
 void reverse_array(int *a, int n)
-{ 
+{
 	int i;
 	int t;
 
